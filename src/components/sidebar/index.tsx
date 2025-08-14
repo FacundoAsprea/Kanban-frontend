095a67c4c903206eb1f6c513a00e2b34b069980b
FacundoAsprea <facundoasprea09@gmail.com>
2025-08-14 20:16:37 -0300

    Boton de github agregado

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,8 @@
+import { FaGithub } from "react-icons/fa"
 export default function Sidebar() {
     return (
-        <nav className='flex-col min-h-screen w-[50px] border-r-1 border-borderMain hidden md:flex' />
+        <nav className='flex-col items-center py-10 min-h-screen w-[50px] border-r-1 border-borderMain hidden md:flex'>
+            <a href="https://github.com/FacundoAsprea" target="_blank"><FaGithub color="white" size="20px" className="hover:fill-blue-500"/></a>
+        </nav>
     )
 }
